b759949006537603a1acb84e1cedd32efd821bc4
root <root@ip-172-31-46-19.ap-south-1.compute.internal>
2025-07-16 05:58:34 +0000

    javaproject

---
+++ javaproject/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end("Hello from Nirmal's Server!\n");
+  } else if (req.url === '/status') {
+    res.end("Status: OK, App is Running!\n");
+  } else {
+    res.statusCode = 404;
+    res.end("404 Not Found\n");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`✅ Server running at http://${hostname}:${port}/`);
+});
